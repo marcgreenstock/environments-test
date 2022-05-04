42a5f10c6e734a5baf17b56c4bc8c499738d79c3
Marc Greenstock <marc.greenstock@vercel.com>
2022-05-04 10:45:20 +0200

    testing custom actions

---
+++ .github/actions/region-matrix/index.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core');
+
+const regionsMap = {
+  'env1': 'region-1',
+  'env2': 'region-2',
+  'env3': 'region-3',
+  'env4': 'region-4',
+  'env5': 'region-5',
+};
+
+const inputRegions = core.getInput('regions', { required: true }).split(' ');
+const selectedRegions = Object.entries(regionsMap).reduce((result, [key, val]) => {
+  if (inputRegions.includes(key)) {
+    return [...result, val];
+  } else {
+    return result;
+  }
+}, []);
+core.setOutput('regions', selectedRegions);
